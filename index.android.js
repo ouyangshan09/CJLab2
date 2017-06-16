af5eac78b72d075c8a661a153dc748cfc609e63f
ouyangshan09 <ouyangshan09@gmail.com>
2017-06-16 17:30:25 +0800

    1、移动练习代码

--- index.android.js
+++ index.android.js
@@ -5,61 +5,10 @@
  */
 
 import React, { Component } from 'react';
-import PropTypes from 'prop-types';
 import {
-    AppRegistry,
-    StyleSheet,
-    Image,
-    Text,
-    TextInput,
-    View,
-    ScrollView,
-    FlatList,
-    SectionList,
-    Button,
+    AppRegistry
 } from 'react-native';
-import { StackNavigator } from 'react-navigation';
-import * as Http from './app/utils/http';
+
 import Launcher from './app/Launcher';
 
 AppRegistry.registerComponent('CJLab2', () => Launcher);
-
-// export default class CJLab2 extends Component {
-//   render() {
-//     return (
-//       <View style={styles.container}>
-//         <Text style={styles.welcome}>
-//           Welcome to React Native!
-//         </Text>
-//         <Text style={styles.instructions}>
-//           To get started, edit index.android.js
-//         </Text>
-//         <Text style={styles.instructions}>
-//           Double tap R on your keyboard to reload,{'\n'}
-//           Shake or press menu button for dev menu
-//         </Text>
-//         <Text>Create by CJ Ouyang</Text>
-//       </View>
-//     );
-//   }
-// }
-//
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     justifyContent: 'center',
-//     alignItems: 'center',
-//     backgroundColor: '#F5FCFF',
-//   },
-//   welcome: {
-//     fontSize: 20,
-//     textAlign: 'center',
-//     margin: 10,
-//   },
-//   instructions: {
-//     textAlign: 'center',
-//     color: '#333333',
-//     marginBottom: 5,
-//   },
-// });
-// AppRegistry.registerComponent('CJLab2', () => CJLab2);
